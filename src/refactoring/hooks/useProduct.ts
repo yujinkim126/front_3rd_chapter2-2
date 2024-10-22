edd272c5ae0e0d74e157279e41a08e8453e5be28
wowyj26 <wowyj26@naver.com>
2024-10-22 09:56:14 +0900

    feat: useProducts 훅 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,30 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useEffect, useState } from "react";
+import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>([]);
+
+  // 제품을 업데이트할 수 있다.
+  const updateProduct = (product: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === product.id ? product : p))
+    );
+  };
+
+  // 새로운 제품을 추가할 수 있다.
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
+  // 특정 제품으로 초기화할 수 있다.
+  useEffect(() => {
+    setProducts(initialProducts);
+  }, [initialProducts]);
+
+  return {
+    products,
+    // getRemainingStock,
+    updateProduct,
+    addProduct,
+  };
 };
