4cba260fd1ee90a19919532472ea390466a72885
wowyj26 <wowyj26@naver.com>
2024-10-22 09:45:01 +0900

    refactor:useCoupon 훅 구현 및 쿠폰 로직 분리

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
 import { Coupon } from "../../types.ts";
-import { useState } from "react";
 
+// 쿠폰 적용 custom hook
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>([]);
+  const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
+
+  const applyCoupon = (index: number) => {
+    const coupon = coupons[index];
+    // 쿠폰 선택 시 쿠폰 상태 변경
+    setSelectedCoupon(coupon || null);
+  };
+
+  useEffect(() => {
+    if (initialCoupons && initialCoupons.length > 0) {
+      // 초기 쿠폰 데이터 설정
+      setCoupons(initialCoupons);
+    }
+  }, [initialCoupons]);
+
+  return {
+    coupons,
+    selectedCoupon,
+    applyCoupon,
+  };
 };
